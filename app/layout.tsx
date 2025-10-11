51cc8acff9a5aa56805e3d731d252550ea65a562
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-10-11 03:18:18 -0500

    change logo and icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,37 @@
-import type React from "react"
-import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-})
+});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-})
+});
 
 export const metadata = {
-  title: "Latido - Tu camino a la vida",
-  description: "Aplicación de control prenatal con educación, ejercicios e IA",
-    generator: 'v0.app'
-}
+  title: "Latidos - Tu camino a la vida",
+  description: "Aplicación de control prenatal con educación",
+  generator: "v0.app",
+  icons: {
+    icon: "/logo.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="es" className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html
+      lang="es"
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
       <body>{children}</body>
     </html>
-  )
+  );
 }
